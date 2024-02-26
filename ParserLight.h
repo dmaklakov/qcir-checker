04886e02a59df4f0ee9fda0dfe4ed2816c3c0584
Dmitry Maklakov <dmitry.maklakov@netural.com>
2024-02-26 12:07:00 +0100

    added ankerl for light

--- ParserLight.h
+++ ParserLight.h
@@ -1,20 +1,20 @@
 #if !defined(COCO_PARSER_LIGHT_H__)
 #define COCO_PARSER_LIGHT_H__
 
-#include <unordered_set>
-#include <unordered_map>
+#include <ankerl/unordered_dense.h>
 
 #include "Scanner.h"
 #include "Parser.h"
 
 using namespace std;
 
-class ParserLight : public Parser {
+class ParserLight : public Parser
+{
 
 public:
-	unordered_set<size_t> unresolved_variables;
+	ankerl::unordered_dense::set<size_t> unresolved_variables;
 
-	unordered_set<size_t> gate_variables_set;
+	ankerl::unordered_dense::set<size_t> gate_variables_set;
 
 	ParserLight(Scanner *scanner, bool check_for_cleansed);
 	~ParserLight();
