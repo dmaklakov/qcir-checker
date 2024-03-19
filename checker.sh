d459a842d204ae1d0fc4603a3727a9aa92078c0e
Dmitry Maklakov <dimator55@gmail.com>
2024-03-19 21:37:34 +0100

    chore: generated new version

---
+++ checker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Set the directory path
+directory="qcir"
+
+# Iterate over files in the directory
+> ".vscode/final.log"
+for file in "$directory"/*; do
+    # Check if the file size is smaller than 100 Mb
+    if [ $(stat -c %s "$file") -lt 104857600 ]; then
+        # Call the generated program with the file
+        # Run qcir.exe with the specified options and redirect output to a file
+        ./qcir.exe "$file" -cleansed -light > ".vscode/a.log" 2>&1
+
+        code=$?
+        echo "Return code for file: $file is $code" >> ".vscode/final.log"
+        echo "Return code for file: $file is $code"
+        # Check the return code
+        if [ $code -ne 0 ]; then
+            echo "Return code is not 0 for file: $file" >> ".vscode/final.log"
+        fi
+    fi
+done
